385442f7f87d36c35adf3c39bf4c05c0a722d009
Rishi Panchal <45925293+rpanch1@users.noreply.github.com>
2021-07-29 16:31:14 -0500

    Can create admin account from register using email admin@email

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -2,13 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-// const httpOptions = {
-//   headers: new HttpHeaders({
-//     'Content-Type': 'application/json',
-//     'Authorization': `Bearer ${localStorage.getItem('token')}`
-//   })
-// }
-
 @Injectable({
   providedIn: 'root'
 })
